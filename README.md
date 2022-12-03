3e69a0b2e9648748d7e83ea716389e5f419e6607
eva-pomposo <evabartolomeu@ua.pt>
2022-12-03 19:44:38 +0000

    put more pretty barplot

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Data visualization website for Netflix's content.
 
 Refrences 
 * https://codepen.io/bwiacek/pen/pXZEvK
+* https://d3-graph-gallery.com/graph/choropleth_hover_effect.html
+* https://d3-graph-gallery.com/graph/barplot_button_data_simple.html
+* https://perials.github.io/responsive-bar-chart-with-d3/
+* https://codesandbox.io/s/50wzl?file=/src/index.js
--- dataflix/src/components/AgeRestriction/AgeRestriction.js
+++ dataflix/src/components/AgeRestriction/AgeRestriction.js
@@ -2,46 +2,64 @@ import "./AgeRestriction.css";
 import * as d3 from "d3";
 import * as d3Selection from "d3-selection";
 import { useRef, useState } from "react";
-import Select from 'react-select';
+import Select from "react-select";
+import makeAnimated from 'react-select/animated';
+
+const animatedComponents = makeAnimated();
 
 function AgeRestriction(props) {
-  const colourOptions = [
-    { value: 'movies', label: 'Movies'},
-    { value: 'tvShows', label: 'Tv Shows'},
-    ];
-
+  const views_options = [
+    { value: "movies", label: "Movies" },
+    { value: "tvShows", label: "Tv Shows" },
+  ];
+  
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      color: state.isSelected ? 'black' : 'black',
+    }),
+  }
   console.log("Data");
   console.log(props.data);
-  const [values, setValues] = useState(props.data);
+  const [values] = useState(props.data);
 
-  var data1 = [];
-  var data2 = [];
+  var data_movies_shows = [];
+  var data_movies = [];
+  var data_shows = [];
   var ages_restrictions = [];
 
   for (let i = 0; i < values.length; i++) {
     let age_rating = values[i].age_rating;
     if (age_rating !== "" && !age_rating.includes("min")) {
       if (ages_restrictions.includes(age_rating)) {
-        data1.find((x) => x.group === age_rating).value++;
+        data_movies_shows.find((x) => x.group === age_rating).value++;
         if (values[i].type === "Movie") {
-          if (data2.find((x) => x.group === age_rating)) {
-            data2.find((x) => x.group === age_rating).value++;
+          if (data_movies.find((x) => x.group === age_rating)) {
+            data_movies.find((x) => x.group === age_rating).value++;
           } else {
-            data2.push({ group: age_rating, value: 1 });
+            data_movies.push({ group: age_rating, value: 1 });
+          }
+        } else {
+          if (data_shows.find((x) => x.group === age_rating)) {
+            data_shows.find((x) => x.group === age_rating).value++;
+          } else {
+            data_shows.push({ group: age_rating, value: 1 });
           }
         }
       } else {
         ages_restrictions.push(age_rating);
-        data1.push({ group: age_rating, value: 1 });
+        data_movies_shows.push({ group: age_rating, value: 1 });
         if (values[i].type === "Movie") {
-          data2.push({ group: age_rating, value: 1 });
+          data_movies.push({ group: age_rating, value: 1 });
+        }else{
+          data_shows.push({ group: age_rating, value: 1 });
         }
       }
     }
   }
 
-  console.log(data1);
-  console.log(data2);
+  console.log(data_movies_shows);
+  console.log(data_movies);
 
   const d3BarChart = useRef();
   const d3G = useRef();
@@ -50,8 +68,8 @@ function AgeRestriction(props) {
 
   // set the dimensions and margins of the graph
   var margin = { top: 30, right: 30, bottom: 70, left: 60 },
-    width = (460 - margin.left - margin.right) * (5/2),
-    height = (400 - margin.top - margin.bottom) * (5/2);
+    width = (460 - margin.left - margin.right) * (7 / 3),
+    height = (400 - margin.top - margin.bottom) * (7 / 3);
 
   // append the svg object to the body of the page
   var svg = d3Selection
@@ -71,14 +89,41 @@ function AgeRestriction(props) {
   var g2 = d3Selection
     .select(d3G2.current)
     .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x));
 
   // Add Y axis
   var y = d3.scaleLinear().domain([0, 3205]).range([height, 0]);
   var g3 = d3Selection
     .select(d3G3.current)
     .attr("class", "myYaxis")
-      .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y));
+
+  // xlabel and ylabel
+  g1.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width / 2)
+    .attr("y", height + margin.top + 20)
+    .text("Age Restriction");
+
+  g1.append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left + 15)
+    .attr("x", -margin.top - height / 2 + 150)
+    .text("Number of Movies/TV Shows");
+
+  // create tooltip element
+  const tooltip = d3Selection
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0)
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("padding", "15px")
+    .style("background", "rgba(0,0,0,0.6)")
+    .style("border-radius", "5px")
+    .style("color", "#fff");
 
   // A function that create / update the plot for a given variable:
   function update(data) {
@@ -101,23 +146,54 @@ function AgeRestriction(props) {
         return height - y(d.value);
       })
       .attr("fill", "#69b3a2");
+
+    g1.selectAll("rect")
+      .on("mouseover", function (d) {
+        tooltip.transition().duration(200).style("opacity", 0.9);
+        tooltip.html(d.value);
+        d3Selection.select(this).style("fill", "#b71a20");
+      }) // show tooltip
+      .on("mousemove", function (d) {
+        tooltip
+          .style("top", d3Selection.event.pageY - 10 + "px")
+          .style("left", d3Selection.event.pageX + 10 + "px");
+      })
+      .on("mouseout", function (d) {
+        tooltip.transition().duration(500).style("opacity", 0);
+        d3Selection.select(this).style("fill", "#D81F26");
+      }); // hide tooltip
   }
 
   // Initialize the plot with the first dataset
-  update(data1);
+  update(data_movies_shows);
 
   return (
     <>
       <h1>Number of Movies and TV Shows by Age Group</h1>
       <Select
-    defaultValue={colourOptions}
-    isMulti
-    options={colourOptions}
-    className="basic-multi-select"
-    classNamePrefix="select"
-  />
-      <button onClick={() => update(data1)}>Variable 1</button>
-      <button onClick={() => update(data2)}>Variable 2</button>
+        defaultValue={views_options}
+        styles={customStyles}
+        components={animatedComponents}
+        closeMenuOnSelect={false}
+        isMulti
+        options={views_options}
+        className="basic-multi-select"
+        classNamePrefix="select"
+        onChange={(e) => {
+          if (e.length === 1) {
+            if (e[0].value === "movies") {
+              update(data_movies);
+            } else {
+              update(data_shows);
+            }
+          } else if (e.length === 0) {
+            update([]);
+          }
+          else {
+            update(data_movies_shows);
+          }
+        }}
+      />
 
       <div className="barChart" id="my_dataviz">
         <svg ref={d3BarChart}>
